8ff4996c63062ec55220508752d5d1cf11eae0c7
Friedel Schön <derfriedmundschoen@gmail.com>
2026-01-31 19:46:47 +0100

    fix no-value in cname

--- stringer.go
+++ stringer.go
@@ -2,78 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Stringer is a tool to automate the creation of methods that satisfy the fmt.Stringer
-// interface. Given the name of a (signed or unsigned) integer type T that has constants
-// defined, stringer will create a new self-contained Go source file implementing
-//
-//	func (t T) String() string
-//
-// The file is created in the same package and directory as the package that defines T.
-// It has helpful defaults designed for use with go generate.
-//
-// Stringer works best with constants that are consecutive values such as created using iota,
-// but creates good code regardless. In the future it might also provide custom support for
-// constant sets that are bit patterns.
-//
-// For example, given this snippet,
-//
-//	package painkiller
-//
-//	type Pill int
-//
-//	const (
-//		Placebo Pill = iota
-//		Aspirin
-//		Ibuprofen
-//		Paracetamol
-//		Acetaminophen = Paracetamol
-//	)
-//
-// running this command
-//
-//	stringer -type=Pill
-//
-// in the same directory will create the file pill_string.go, in package painkiller,
-// containing a definition of
-//
-//	func (Pill) String() string
-//
-// That method will translate the value of a Pill constant to the string representation
-// of the respective constant name, so that the call fmt.Print(painkiller.Aspirin) will
-// print the string "Aspirin".
-//
-// Typically this process would be run using go generate, like this:
-//
-//	//go:generate stringer -type=Pill
-//
-// If multiple constants have the same value, the lexically first matching name will
-// be used (in the example, Acetaminophen will print as "Paracetamol").
-//
-// With no arguments, it processes the package in the current directory.
-// Otherwise, the arguments must name a single directory holding a Go package
-// or a set of Go source files that represent a single Go package.
-//
-// The -type flag accepts a comma-separated list of types so a single run can
-// generate methods for multiple types. The default output file is t_string.go,
-// where t is the lower-cased name of the first type listed. It can be overridden
-// with the -output flag.
-//
-// Types can also be declared in tests, in which case type declarations in the
-// non-test package or its test variant are preferred over types defined in the
-// package with suffix "_test".
-// The default output file for type declarations in tests is t_string_test.go with t picked as above.
-//
-// The -linecomment flag tells stringer to generate the text of any line comment, trimmed
-// of leading spaces, instead of the constant name. For instance, if the constants above had a
-// Pill prefix, one could write
-//
-//	PillAspirin // Aspirin
-//
-// to suppress it in the output.
-//
-// The -trimprefix flag specifies a prefix to remove from the constant names
-// when generating the string representations. For instance, -trimprefix=Pill
-// would be an alternative way to ensure that PillAspirin.String() == "Aspirin".
 package main
 
 import (
@@ -368,7 +296,7 @@ func (g *Generator) generate(typeName string, values []Value) {
 		// For each value, you'll get 4 lines of source-code. This
 		// might overfloat the resulting file and we're choosing to
 		// generate a less verbose technique.
-		if len(values) <= 200 {
+		if len(values) <= 500 {
 			g.buildLookup(typeName, values)
 		} else {
 			g.buildLookupMap(typeName, values)
@@ -475,13 +403,14 @@ func (b byValue) Less(i, j int) bool {
 }
 
 func unwrapParen(e ast.Expr) ast.Expr {
-	for {
+	for e != nil {
 		p, ok := e.(*ast.ParenExpr)
 		if !ok {
 			return e
 		}
 		e = p.X
 	}
+	return nil
 }
 
 func getCName(expr ast.Expr) string {
@@ -498,6 +427,19 @@ func getCName(expr ast.Expr) string {
 		return ""
 	}
 	return name
+}
+
+func valueExpr(vspec *ast.ValueSpec, ni int) ast.Expr {
+	if len(vspec.Values) == 0 {
+		return nil
+	}
+	if len(vspec.Values) == 1 {
+		return vspec.Values[0]
+	}
+	if ni < len(vspec.Values) {
+		return vspec.Values[ni]
+	}
+	return nil
 }
 
 // genDecl processes one declaration clause.
@@ -580,7 +522,7 @@ func (f *File) genDecl(node ast.Node) bool {
 			}
 			cName := ""
 			if f.cNames {
-				cName = getCName(vspec.Values[ni])
+				cName = getCName(valueExpr(vspec, ni))
 			}
 			v := Value{
 				originalName: name.Name,
